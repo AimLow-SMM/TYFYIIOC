e66e6e7e2b09bbe793ad0f9ee60bfe5341d663fc
shawn <shawnmmck@gmail.com>
2022-01-09 23:01:17 -0600

    replaced Rigidbody with CharacterController

--- OfficeSpace/Assets/TeskePrefabs/Scripts/FPSController.cs
+++ OfficeSpace/Assets/TeskePrefabs/Scripts/FPSController.cs
@@ -14,65 +14,39 @@ public class FPSController : MonoBehaviour
     public float maxPitch = 60f;
     private float cameraY;
     private float cameraX;
+    private Vector3 inputVector;
+    private Vector3 moveVector;
     private Camera cam;
-    private BoxCollider playerCollider;
-    Vector3 oldPosition;
-    private Rigidbody rb;
+    // private BoxCollider playerCollider;
+    // Vector3 oldPosition;
+    // private Rigidbody rb;
+    public CharacterController controller;
 
 
     [SerializeField] private LayerMask clickablesLayer;
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         cam = Camera.main;
-        playerCollider = GetComponent<BoxCollider>();
-        rb = GetComponent<Rigidbody>();
+        // playerCollider = GetComponent<BoxCollider>();
+        // rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Rotation();
+        // Should Rotation() be inside of the .working if statement?
+        Rotation(); // FPS camera and body rotation
 
         if (currentState != PlayerStates.WORKING)
         {
-            Movement();
+            Movement(); // Classic WASD movement
+            Interactable(); // Check for interactable objects
         }
-
     }
-
-    void Movement()
+    void Interactable()
     {
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(cam.transform.forward.x, 0f, cam.transform.forward.z).normalized * playerSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(cam.transform.forward.x, 0f, cam.transform.forward.z).normalized * playerSpeed * Time.deltaTime * -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(cam.transform.right.x, 0f, cam.transform.right.z).normalized * playerSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(cam.transform.right.x, 0f, cam.transform.right.z).normalized * playerSpeed * Time.deltaTime * -1;
-        }
-        else
-        {
-            transform.position += new Vector3(0, 0, 0) * playerSpeed * Time.deltaTime;
-        }
-        
-
-
-
-        //Check for interactable objects
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -84,9 +58,45 @@ public class FPSController : MonoBehaviour
                 if (currentInteraction != null)
                 {
                     currentInteraction.Interact();
- 
                 }
             }
+        }
+    }
+    void Movement()
+    {
+        inputVector = new Vector3(0f, 0f, 0f);
+
+        // Input.GetAxis() Takes a bit of setup but works with WASD, arrow keys, joystick, controller
+        // float x = Input.GetAxis("Horizontal"); 
+        // float z = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += new Vector3(0f, 0f, 1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += new Vector3(0f, 0f, -1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += new Vector3(1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += new Vector3(-1f, 0f, 0f);
+        }
+        inputVector = inputVector.normalized;
+
+        moveVector = transform.right * inputVector.x + transform.forward * inputVector.z;
+
+        controller.Move(moveVector * playerSpeed * Time.deltaTime);
+
+        // To stop player from climbing on objects
+        float badHardCodedfloorHeight = 1.39f;
+        if (transform.position.y > badHardCodedfloorHeight)
+        {
+            transform.position = new Vector3(transform.position.x, badHardCodedfloorHeight, transform.position.z);
         }
     }
 
@@ -100,39 +110,39 @@ public class FPSController : MonoBehaviour
     }
 
 
-/*    public void PlayerIsUnsafe()
-    {
-        currentState = PlayerStates.UNSAFE;
-        Debug.Log("Player is UNSAFE");
-    }
+    /*    public void PlayerIsUnsafe()
+        {
+            currentState = PlayerStates.UNSAFE;
+            Debug.Log("Player is UNSAFE");
+        }
 
-    public void PlayerIsSafe()
-    {
-        currentState = PlayerStates.SAFE;
-        Debug.Log("Player is SAFE");
-        
-    }
+        public void PlayerIsSafe()
+        {
+            currentState = PlayerStates.SAFE;
+            Debug.Log("Player is SAFE");
 
-    public void PlayerIsWorking()
-    {
-        currentState = PlayerStates.WORKING;
-        Debug.Log("Player is WORKING");
-    }
-*/
-/*
-    public void SetCameraForMinigame(Vector3 newPosition)
-    {
-        oldPosition = transform.position;
-        playerCollider.enabled = !playerCollider.enabled;
-        transform.position = newPosition;
-    }
+        }
 
-    public void ReturnPlayerAfterMinigame()
-    {
-        transform.position = oldPosition;
-        //playerCollider.enabled = playerCollider.enabled;
-        FindObjectOfType<GameStatesManager>().ResetToSafe();
-        
-    }
-*/
+        public void PlayerIsWorking()
+        {
+            currentState = PlayerStates.WORKING;
+            Debug.Log("Player is WORKING");
+        }
+    */
+    /*
+        public void SetCameraForMinigame(Vector3 newPosition)
+        {
+            oldPosition = transform.position;
+            playerCollider.enabled = !playerCollider.enabled;
+            transform.position = newPosition;
+        }
+
+        public void ReturnPlayerAfterMinigame()
+        {
+            transform.position = oldPosition;
+            //playerCollider.enabled = playerCollider.enabled;
+            FindObjectOfType<GameStatesManager>().ResetToSafe();
+
+        }
+    */
 }
